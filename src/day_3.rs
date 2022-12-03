25e0c4ce08eaf521aa8dbff6aeb097fd3b14cb83
pierrick-delamotte <pierrick.delamotte@gmail.com>
2022-12-03 15:43:00 +0100

    refacto

--- src/day_3.rs
+++ src/day_3.rs
@@ -7,12 +7,11 @@ pub(crate) fn day_3_1() {
     if let Ok(lines) = read_lines("./data/input_3.txt") {
         for line in lines {
             if let Ok(rucksacks) = line {
-                let packages = rucksacks.chars().collect::<Vec<char>>();
-
-                let first_compartment = packages[0..packages.len() / 2].to_vec();
-                let second_compartment = packages[packages.len() / 2..packages.len()].to_vec();
-
-                let duplicate_package = first_compartment.intersect(second_compartment);
+                let compartments = rucksacks.chars().collect::<Vec<char>>()
+                    .chunks(rucksacks.len() / 2)
+                    .map(|x| Vec::from(x))
+                    .collect::<Vec<Vec<char>>>();
+                let duplicate_package = compartments[0].intersect(compartments[1].clone());
                 for package in duplicate_package {
                     let package_value = package as usize;
                     priority_sum += if package_value > 96 { package_value - 96 } else { package_value - 64 + 26 };
@@ -31,11 +30,8 @@ pub(crate) fn day_3_2() {
         let elk_groups = elks.chunks(3).collect::<Vec<&[String]>>();
 
         for elk_group in &elk_groups {
-            let first_rucksack = elk_group[0].chars().collect::<Vec<char>>();
-            let second_rucksack = elk_group[1].chars().collect::<Vec<char>>();
-            let third_rucksack = elk_group[2].chars().collect::<Vec<char>>();
-
-            let tag = first_rucksack.intersect(second_rucksack).intersect(third_rucksack);
+            let rucksacks = elk_group.iter().map(|x| x.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+            let tag = rucksacks[0].intersect(rucksacks[1].clone()).intersect(rucksacks[2].clone());
             for package in tag {
                 let package_value = package as usize;
                 priority_sum += if package_value > 96 { package_value - 96 } else { package_value - 64 + 26 };
