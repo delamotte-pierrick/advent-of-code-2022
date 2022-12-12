07668c9f00eba22a67f9810e516176d31ac92393
pierrick-delamotte <pierrick.delamotte@gmail.com>
2022-12-13 00:43:34 +0100

    refacto day 12

--- src/day_12.rs
+++ src/day_12.rs
@@ -3,72 +3,32 @@ use std::string::String;
 use crate::utils::read_lines;
 
 pub(crate) fn day_12_1() {
-    let maze = parse_input("./data/input_12.txt");
-    let mut start: Option<String> = None;
-    let mut end: Option<Vec<String>> = None;
+    let (maze, start, end) = parse_input("./data/input_12.txt", 'S', 'E');
 
-    for (i, row) in maze.iter().enumerate() {
-        if row.contains(&'S') {
-            for (j, cell) in row.iter().enumerate() {
-                if *cell == 'S' {
-                    start = Option::from(i.to_string() + "," + &j.to_string());
-                }
-            }
-        }
-
-        if row.contains(&'E') {
-            for (j, cell) in row.iter().enumerate() {
-                if *cell == 'E' {
-                    end = Option::from(Vec::from([i.to_string() + "," + &j.to_string()]));
-                }
-            }
-        }
-    }
-    if start.is_none() || end.is_none() {
+    if start.is_none() || end.len() > 0 {
         panic!("No start or end found");
     }
 
-    let path = pathfinder_bfs(maze.clone(), start.clone().unwrap(), end.clone().unwrap(), false);
-
-    // visualize_maze(maze.clone(), path.clone());
+    let path = pathfinder_bfs(maze, start.unwrap(), end, false);
 
     println!("Shortest path is: {} cell long", path.len());
 }
 
 pub(crate) fn day_12_2() {
-    let maze = parse_input("./data/input_12.txt");
-    let mut start: Option<String> = None;
-    let mut end: Vec<String> = Vec::new();
+    let (maze, start, end) = parse_input("./data/input_12.txt", 'E', 'a');
 
-    for (i, row) in maze.iter().enumerate() {
-        if row.contains(&'E') {
-            for (j, cell) in row.iter().enumerate() {
-                if *cell == 'E' {
-                    start = Option::from(i.to_string() + "," + &j.to_string());
-                }
-            }
-        }
-
-        if row.contains(&'a') || row.contains(&'S') {
-            for (j, cell) in row.iter().enumerate() {
-                if ['S', 'a'].contains(cell) {
-                    end.push(i.to_string() + "," + &j.to_string());
-                }
-            }
-        }
-    }
     if start.is_none() || end.len() == 0 {
         panic!("No start or end found");
     }
 
-    let path = pathfinder_bfs(maze.clone(), start.clone().unwrap(), end.clone(), true);
+    let path = pathfinder_bfs(maze, start.unwrap(), end, true);
 
-    // visualize_maze(maze.clone(), path.clone());
+    // visualize_maze(maze.clone(), path.clone(), Option::from(start.clone().unwrap()), end.clone());
 
     println!("Shortest path to lowest elevation is: {} cell long", path.len());
 }
 
-fn pathfinder_bfs(maze: Vec<Vec<char>>, start: String, end: Vec<String>, is_from_end : bool) -> Vec<(String, String)> {
+fn pathfinder_bfs(maze: Vec<Vec<char>>, start: String, end: Vec<String>, is_from_end: bool) -> Vec<(String, String)> {
     let mut queue: VecDeque<(Vec<(String, String)>, String)> = VecDeque::from([(Vec::new(), start.clone())]);
     let mut visited: Vec<String> = Vec::new();
     let graph = maze_to_graph(maze.clone(), is_from_end);
@@ -95,64 +55,62 @@ fn pathfinder_bfs(maze: Vec<Vec<char>>, start: String, end: Vec<String>, is_from
     panic!("No path found");
 }
 
-fn maze_to_graph(maze: Vec<Vec<char>>, is_from_end : bool) -> HashMap<String, Vec<(String, String)>> {
+fn maze_to_graph(maze: Vec<Vec<char>>, is_from_end: bool) -> HashMap<String, Vec<(String, String)>> {
     let mut graph: HashMap<String, Vec<(String, String)>> = HashMap::new();
-    let height = maze.len();
-    let width = if height > 0 { maze[0].len() } else { 0 };
-
-    for i in 0..height {
-        for j in 0..width {
-            let cell_address = i.to_string() + "," + &j.to_string();
+    (0..maze.len()).map(|i| i).for_each(|i| {
+        (0..maze[0].len()).for_each(|j| {
+            let i = i as i32;
+            let j = j as i32;
+            let cell_address = format!("{},{}", i, j);
 
             if !graph.contains_key(cell_address.as_str()) {
                 graph.insert(cell_address.clone(), Vec::new());
             }
-        }
-    }
 
-    for (i, row) in maze.iter().enumerate() {
-        for (j, cell) in row.iter().enumerate() {
-            let cell_value = replace_char(*cell) as usize;
+            let cell_value = replace_char(maze[i as usize][j as usize]) as usize;
 
-            let i = i as i32;
-            let j = j as i32;
+            [(i + 1, j, "↑"), (i - 1, j, "↓"), (i, j + 1, "←"), (i, j - 1, "→")].into_iter().for_each(|(x, y, direction)| {
+                if x >= 0 && x < maze.len() as i32 && y >= 0 && y < maze[0].len() as i32 {
+                    let neighbour_value = replace_char(maze[x as usize][y as usize]) as usize;
 
-            [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)].iter().for_each(|(x, y)| {
-                if *x >= 0 && *x < height as i32 && *y >= 0 && *y < width as i32 {
-                    let neighbour_value = replace_char(maze[*x as usize][*y as usize]) as usize;
-                    let cell_address = i.to_string() + "," + &j.to_string();
-                    let neighbour_address = x.to_string() + "," + &y.to_string();
+                    let mut can_go_node = cell_value + 1 == neighbour_value || cell_value >= neighbour_value;
+                    if is_from_end {
+                        can_go_node = cell_value - 1 == neighbour_value || cell_value <= neighbour_value;
+                    }
 
-                    let from_end = cell_value - 1 == neighbour_value || cell_value <= neighbour_value;
-                    let from_start = cell_value + 1 == neighbour_value || cell_value >= neighbour_value;
-
-                    if (is_from_end && from_end) || (!is_from_end && from_start) {
-                        let direction = match (i as i32 - *x as i32, j as i32 - *y as i32) {
-                            (1, 0) => "↑",
-                            (-1, 0) => "↓",
-                            (0, 1) => "←",
-                            (0, -1) => "→",
-                            _ => panic!("Invalid direction")
-                        };
-                        graph.get_mut(cell_address.as_str()).unwrap().push((direction.parse().unwrap(), neighbour_address));
+                    if can_go_node {
+                        graph.get_mut(cell_address.as_str()).unwrap().push((direction.to_string(), format!("{},{}", x, y)));
                     }
                 }
             });
-        }
-    }
+        });
+    });
 
     return graph;
 }
 
-fn parse_input(file_path: &str) -> Vec<Vec<char>> {
+fn parse_input(file_path: &str, start_char: char, end_char: char) -> (Vec<Vec<char>>, Option<String>, Vec<String>) {
     let mut maze: Vec<Vec<char>> = Vec::new();
+    let mut start: Option<String> = None;
+    let mut end: Vec<String> = Vec::new();
+
     if let Ok(lines) = read_lines(file_path) {
-        for line in lines {
+        lines.for_each(|line| {
             maze.push(line.unwrap().chars().collect());
-        }
+        });
     }
 
-    return maze;
+    (0..maze.len()).for_each(|i| {
+        (0..maze[i].len()).for_each(|j| {
+            if maze[i][j] == start_char {
+                start = Some(format!("{},{}", i, j));
+            } else if maze[i][j] == end_char {
+                end.push(format!("{},{}", i, j));
+            }
+        });
+    });
+
+    return (maze, start, end);
 }
 
 fn replace_char(char: char) -> char {
@@ -163,19 +121,31 @@ fn replace_char(char: char) -> char {
     }
 }
 
-fn visualize_maze(maze: Vec<Vec<char>>, path: Vec<(String, String)>) {
-    let mut maze = maze.clone().into_iter()
-        .map(|row| row.into_iter().map(|cell| if ['S', 'E'].contains(&cell) { cell } else { ' ' }).collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+fn visualize_maze(maze: Vec<Vec<char>>, path: Vec<(String, String)>, start: Option<String>, ends: Vec<String>) {
+    let mut maze: Vec<Vec<String>> = (0..maze.len())
+        .into_iter()
+        .map(|i| (0..maze[i].len())
+            .into_iter()
+            .map(|_| " ".to_string())
+            .collect::<Vec<String>>())
+        .collect();
+
+    //Write start and end
+    if let Some(start) = start {
+        let start: Vec<usize> = start.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+        maze[start[0]][start[1]] = "S".to_string();
+    }
+    for end in ends {
+        let end: Vec<usize> = end.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+        maze[end[0]][end[1]] = "E".to_string();
+    }
 
     for (direction, cell) in path {
-        let coords: Vec<&str> = cell.split(",").collect();
-        let x = coords[0].parse::<usize>().unwrap();
-        let y = coords[1].parse::<usize>().unwrap();
-        maze[x][y] = direction.chars().next().unwrap();
+        let coords: Vec<usize> = cell.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+        maze[coords[0]][coords[1]] = direction.chars().next().unwrap().to_string();
     }
 
     for row in maze {
-        println!("{:?}", row);
+        println!("{:?}", row.join(""));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,29 +20,29 @@ fn main() {
     let now = std::time::Instant::now();
     let mut times = Vec::new();
 
-    day_1::day_1_1();
-    day_1::day_1_2();
-    day_2::day_2_1();
-    day_2::day_2_2();
-    day_3::day_3_1();
-    day_3::day_3_2();
-    day_4::day_4_1();
-    day_4::day_4_2();
-    day_5::day_5_1();
-    day_5::day_5_2();
-    day_6::day_6_1();
-    day_6::day_6_2();
-    day_7::day_7_1();
-    day_7::day_7_2();
-    day_8::day_8_1();
-    day_8::day_8_2();
-    day_9::day_9_1();
-    day_9::day_9_2();
-    day_10::day_10_1();
-    day_10::day_10_2();
-    day_11::day_11_1();
-    day_11::day_11_2(); // This day takes a while to run
-    day_12::day_12_1(); // This day takes a while to run
+    // day_1::day_1_1();
+    // day_1::day_1_2();
+    // day_2::day_2_1();
+    // day_2::day_2_2();
+    // day_3::day_3_1();
+    // day_3::day_3_2();
+    // day_4::day_4_1();
+    // day_4::day_4_2();
+    // day_5::day_5_1();
+    // day_5::day_5_2();
+    // day_6::day_6_1();
+    // day_6::day_6_2();
+    // day_7::day_7_1();
+    // day_7::day_7_2();
+    // day_8::day_8_1();
+    // day_8::day_8_2();
+    // day_9::day_9_1();
+    // day_9::day_9_2();
+    // day_10::day_10_1();
+    // day_10::day_10_2();
+    // day_11::day_11_1();
+    // day_11::day_11_2(); // This day takes a while to run
+    // day_12::day_12_1(); // This day takes a while to run
     day_12::day_12_2(); // This day takes a while to run
 
     times.push(now.elapsed());
